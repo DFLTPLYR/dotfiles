bef49bd90e55bb0736fb714793652b02a7bbee91
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-07-03 04:59:20 +0800

    Added more

--- .config/rmpc/scripts/resize.sh
+++ .config/rmpc/scripts/resize.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-notify-send "Current rows: $ROWS"
+# notify-send "Current rows: $ROWS"
 
 if (( ROWS < 50 )); then
     rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/config.ron
-    notify-send "Small config applied"
 elif (( ROWS <= 75 )); then
     rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/medium.ron
-    notify-send "Medium config applied"
 else
     rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/long.ron
-    notify-send "Long config applied"
 fi
