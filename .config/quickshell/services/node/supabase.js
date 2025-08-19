45ff24f104c740732d43cc341f2b3e6a49ac3db8
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-08-19 15:22:02 +0800

    refactor: remove unused Node service files and update README with installation instructions

--- .config/quickshell/services/node/supabase.js
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY);
---
+++ .config/quickshell/todo-service/src/index.ts
@@ -0,0 +1,32 @@
+import { serve } from "bun";
+import { Hono } from "hono";
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
+
+const app = new Hono();
+
+// hehe funny number
+const PORT = process.env.PORT ? Number(process.env.PORT) : 6969;
+
+const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.API_KEY as string
+);
+
+// Rest Api
+app.get("/", (c) => {
+  return c.text("OK", 200);
+});
+
+app.get("/todos", async (c) => {
+  const { data, error } = await supabase.from("todos").select();
+  if (error) return c.json({ error }, 500);
+  return c.json(data);
+});
+
+serve({
+  fetch: app.fetch,
+  port: PORT,
+});
+
+export default app;
