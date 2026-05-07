2d356aa2ca56135b85e334a70613ee0f6d947383
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-05-07 19:47:14 +0800

    refactor(rmpc): simplify config and enhance layout styling
    Remove on_resize/on_song_change hooks and unused layout tabs.
    Add visual styling enhancements to long layout including tab
    bar, custom symbols, progress bar, and scrollbar styling.
    Update cava settings and add rmpcd to niri startup.
    Clean up obsolete utility scripts.

--- .local/bin/resize-player.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-if (( ROWS < 50 )); then
-    rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/config.ron
-elif (( ROWS <= 75 )); then
-    rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/medium.ron
-else
-    rmpc remote --pid "$PID" set config /home/dfltplyr/.config/rmpc/layout/long.ron
-fi
