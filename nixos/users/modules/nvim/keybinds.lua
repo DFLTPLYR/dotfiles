f0f0d59e556ef73a6021af496032ebd4e3cb8cc0
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-06-02 18:53:54 +0800

    feat(nvim): add conditional buffer keybinds and restrict indent to visual

--- nixos/users/modules/nvim/keybinds.lua
+++ nixos/users/modules/nvim/keybinds.lua
@@ -3,8 +3,8 @@ vim.schedule(function()
 	local map = vim.keymap.set
 	-- Additional Keybinds
 	map("i", "jj", "<ESC>", { desc = "exit insert mode" })
-	map({"v", "n"}, "<A-l>", ">gv", { desc = "Indent right" })
-	map({"v", "n"}, "<A-h>", "<gv", { desc = "Indent left" })
+	map("v", "<A-l>", ">gv", { desc = "Indent right" })
+	map("v", "<A-h>", "<gv", { desc = "Indent left" })
 	map("n", "<A-j>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
 	map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 	map("n", "<A-k>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
@@ -16,4 +16,20 @@ vim.schedule(function()
 	nomap("n", "<leader>v")
 	nomap({ "n", "t" }, "<A-h>")
 	nomap({ "n", "t" }, "<A-v>")
+
+  if require("nvconfig").ui.tabufline.enabled then
+    map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
+
+    map("n", "<A-l>", function()
+      require("nvchad.tabufline").next()
+    end, { desc = "buffer goto next" })
+
+    map("n", "<A-h>", function()
+      require("nvchad.tabufline").prev()
+    end, { desc = "buffer goto prev" })
+
+    map("n", "<leader>x", function()
+      require("nvchad.tabufline").close_buffer()
+    end, { desc = "buffer close" })
+  end
 end)
