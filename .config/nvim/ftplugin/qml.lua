d5dce5af63b5c024027e3338088d987169949692
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-06-22 20:49:53 +0800

    fix(nvim): use indent-only folding for qml files
    Skip treesitter-based folding for QML via nvim-ufo and create a
    dedicated ftplugin with indent folding and a 3-level nest limit.

---
+++ .config/nvim/ftplugin/qml.lua
@@ -0,0 +1,5 @@
+vim.opt_local.foldmethod = "indent"
+vim.opt_local.foldnestmax = 3
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -5,6 +5,9 @@ vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 require("ufo").setup({
 	provider_selector = function(bufnr, filetype, buftype)
+		if filetype == "qml" then
+			return { "indent" }
+		end
 		return { "treesitter", "indent" }
 	end,
 })
