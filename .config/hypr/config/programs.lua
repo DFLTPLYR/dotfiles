2f3c40042a657137c9e2d72ac1757961b1997f79
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-07-24 22:13:54 +0800

    fix(hypr): replace quickcli with direct qs IPC in mode binding

--- .config/hypr/config/programs.lua
+++ .config/hypr/config/programs.lua
@@ -6,6 +6,10 @@ menu = "pkill rofi || rofi -show drun -p 'Launch App'"
 clipboard = [[pkill rofi || cliphist list | rofi -dmenu | cliphist decode | wl-copy]]
 browser = "zen"
 editor = "nvim"
-mode = "quickcli launch cycleState"
-selectshot = [[mkdir -p ~/Pictures/Screenshots && grim -g "$(slurp)" - | tee ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy]]
-screenshot = [[mkdir -p ~/Pictures/Screenshots && grim -g "$(slurp -o)" - | tee ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy]]
+
+qs_config = "qs -c reshell ipc call config"
+mode = qs_config .. " cycleState"
+selectshot =
+	[[mkdir -p ~/Pictures/Screenshots && grim -g "$(slurp)" - | tee ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy]]
+screenshot =
+	[[mkdir -p ~/Pictures/Screenshots && grim -g "$(slurp -o)" - | tee ~/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png | wl-copy]]
