d895ccc2e201aa1819d88cd3b9d4a19da9a14a13
DFLTPLYR <gonzales.johncris01@gmail.com>
2026-09-24 20:06:44 +0800

    feat(networking): use AdGuard DNS via systemd-resolved

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -6,4 +6,13 @@
   # Enable networking
   networking.hostName = "nixosBtw"; # Define your hostname.
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "systemd-resolved";
+  networking.networkmanager.insertNameservers = [
+    "94.140.14.14"
+    "94.140.15.15"
+  ];
+  networking.nameservers = [
+    "94.140.14.14"
+    "94.140.15.15"
+  ];
 }
